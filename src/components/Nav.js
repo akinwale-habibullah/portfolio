5e4f764e7730e5974ae89b33ce7ab49854c61c7b
Akinwale Habib <akinwalehabib@hotmail.com>
2021-10-23 00:29:50 +0100

    add nav building block component

---
+++ src/components/Nav.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import {
+  Box,
+  Avatar,
+  Button,
+  useTheme,
+  useMediaQuery
+} from '@mui/material'
+import {
+  MailOutline
+} from '@mui/icons-material'
+import { navContainer, link } from './Nav.module.css'
+
+const Nav = () => {
+  const theme = useTheme()
+  const matches = useMediaQuery(theme.breakpoints.down('sm'))
+  return (
+    <Box
+      className={navContainer} 
+      sx={{
+        pt: {
+          xs: 2,
+          md: 3,
+          lg: 4
+        },
+        pl: {
+          xs: 1,
+          md: 3,
+          lg: 4
+        },
+        pr: {
+          xs: 1,
+          md: 3,
+          lg: 4
+        },
+      }}
+    >
+      <Link to='/' className={link}>
+        <Avatar
+          sx={{ width: 56, height: 56 }}
+        >
+          AH
+        </Avatar>
+      </Link>
+      {
+        matches
+        ? <Avatar
+          sx={{ width: 56, height: 56 }}
+          >
+            <MailOutline />
+          </Avatar>
+        : <Button
+            variant='contained'
+            color='primary'
+          >
+            Contact me
+          </Button>
+      }
+    </Box>
+  )
+}
+
+export default Nav
